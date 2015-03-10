8b485b47858cb5c3c1f8efc9a1c4b6311453622c
Darío Javier Cravero <dario@uxtemple.com>
2015-03-10 17:06:27 +0000

    Update playground.js

--- playground/playground.js
+++ playground/playground.js
@@ -2,5 +2,5 @@ import * as Base from '../'
 window.Base = Base
 
 console.log('Welcome to base playground.')
-console.log('https://github.com/dariocraverob/base')
+console.log('https://github.com/dariocravero/base')
 console.log('Base module', Base)
